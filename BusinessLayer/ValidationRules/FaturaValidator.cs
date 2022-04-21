64cfc088aafbd4889e82a535ebcc7051cffee367
İsmail Altay <ismailaltay3553@gmail.com>
2022-04-21 14:50:12 +0300

    Admin tarafına fatura ekleme ve fatura listeleme eklendi

---
+++ BusinessLayer/ValidationRules/FaturaValidator.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class FaturaValidator : AbstractValidator<Fatura>
+    {
+        public FaturaValidator()
+        {
+            RuleFor(x => x.FaturaId).GreaterThan(0).WithMessage("Fatura Id'si, sıfırndan büyük olmalıdır");
+
+            RuleFor(x => x.FaturaTarihi).GreaterThan(DateTime.Today.AddDays(-10)).WithMessage("Fatura günü, 10 gün veya daha öncesine atanamaz")
+                .NotEmpty().WithMessage("Fatura tarihi boş geçilemez.");
+
+            RuleFor(x => x.FaturaSonOdemeTarihi).GreaterThan(x => x.FaturaTarihi.AddMonths(1)).WithMessage("Fatura son ödeme tarihi, Aidat Tarihinden 1 ay sonrası olmalıdır")
+                .NotEmpty().WithMessage("Fatura son ödeme tarihi boş geçilemez.");
+
+            RuleFor(x => x.FaturaTipi).NotEmpty().WithMessage("Fatura tipi boş geçilemez.");
+
+            RuleFor(x => x.FaturaTutarı).GreaterThan(0).WithMessage("Fatura tutarı sıfırdan büyük olmalıdır.")
+                .NotEmpty().WithMessage("Fatura tutarı boş geçilemez.");
+
+            RuleFor(x => x.KullanıcıId).NotEmpty().WithMessage("Kullanıcı boş geçilemez.").
+                GreaterThan(0).WithMessage("Kullanıcı Id sıfırndan büyük olmalıdır.");
+        }
+    }
+}
--- FaturaYönetimSistemi/Controllers/FaturaController.cs
+++ FaturaYönetimSistemi/Controllers/FaturaController.cs
@@ -1,8 +1,12 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FaturaYönetimSistemi.Controllers
 {
@@ -22,11 +26,33 @@ namespace FaturaYönetimSistemi.Controllers
         [HttpGet]
         public IActionResult AddFatura()
         {
+            KullanıcıManager kullanıcıManager = new(new EFKullanıcıRepository());
+            List<SelectListItem> kullanıcılar = kullanıcıManager.GetAllQuery().
+                                                Select(x => new SelectListItem
+                                                {
+                                                    Text = x.KullanıcıIsım + " " + x.KullanıcıSoyisim,
+                                                    Value = x.KullanıcıId.ToString()
+                                                }).ToList();
+            ViewBag.kullanıcılar = kullanıcılar;
             return View();
         }
         [HttpPost]
         public IActionResult AddFatura(Fatura fatura)
         {
+            FaturaValidator validator = new FaturaValidator();
+            ValidationResult validationResult = validator.Validate(fatura);
+            if (validationResult.IsValid)
+            {
+                manager.AddT(fatura);
+                return RedirectToAction("AdminGetAllFaturas");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
